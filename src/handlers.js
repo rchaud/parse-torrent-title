b6732520a8c90f7817e8fcac1bd0bce694a8854e
TheBeastLT <pauliox@beyond.lt>
2020-02-23 23:17:59 +0100

    adds -logy collections handler

--- src/handlers.js
+++ src/handlers.js
@@ -178,6 +178,7 @@ exports.addDefaults = /** @type Parser */ parser => {
     parser.addHandler("complete", /(?:\bthe\W)?(?:\bcomplete|full|all)\b.*\b(?:series|seasons|collection|episodes|set|pack|movies)\b/i, boolean);
     parser.addHandler("complete", /\b(?:series|seasons|movies)\b[ .]\b(?:complete|collection)\b/i, boolean);
     parser.addHandler("complete", /(?:\bthe\W)?\bultimate\b[ .]\bcollection\b/i, boolean, { skipIfAlreadyFound: false });
+    parser.addHandler("complete", /duology|trilogy|quadr[oi]logy|tetralogy|pentalogy|hexalogy|heptalogy|anthology/i, boolean, { skipIfAlreadyFound: false });
 
     // Language
     parser.addHandler("language", /\bRUS\b/i, lowercase);
--- test/complete.js
+++ test/complete.js
@@ -51,5 +51,50 @@ describe("Parsing complete collection", () => {
         const releaseName = "Harry Potter All Movies Collection 2001-2011 720p Dual KartiKing";
         expect(parse(releaseName)).to.deep.include({ complete: true });
     });
+
+    it("should detect duology", () => {
+        const releaseName = "Snabba.Cash.I-II.Duology.2010-2012.1080p.BluRay.x264.anoXmous";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
+
+    it("should detect trilogy", () => {
+        const releaseName = "Star Wars Original Trilogy 1977-1983 Despecialized 720p";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
+
+    it("should detect quadrology", () => {
+        const releaseName = "The.Wong.Kar-Wai.Quadrology.1990-2004.1080p.BluRay.x264.AAC.5.1-";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
+
+    it("should detect quadrilogy", () => {
+        const releaseName = "Lethal.Weapon.Quadrilogy.1987-1992.1080p.BluRay.x264.anoXmous";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
+
+    it("should detect tetralogy", () => {
+        const releaseName = "X-Men.Tetralogy.BRRip.XviD.AC3.RoSubbed-playXD";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
+
+    it("should detect pentalogy", () => {
+        const releaseName = "Mission.Impossible.Pentalogy.1996-2015.1080p.BluRay.x264.AAC.5.1";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
+
+    it("should detect hexalogy", () => {
+        const releaseName = "Mission.Impossible.Hexalogy.1996-2018.SweSub.1080p.x264-Justiso";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
+
+    it("should detect hexalogy", () => {
+        const releaseName = "American.Pie.Heptalogy.SWESUB.DVDRip.XviD-BaZZe";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
+
+    it("should detect anthalogy", () => {
+        const releaseName = "The Exorcist 1, 2, 3, 4, 5 - Complete Horror Anthology 1973-2005";
+        expect(parse(releaseName)).to.deep.include({ complete: true });
+    });
 });
 
